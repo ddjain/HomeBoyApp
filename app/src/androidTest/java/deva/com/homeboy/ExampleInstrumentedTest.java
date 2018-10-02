b01ae8074c66d7ae68d2695df5ad36b7ecd053f9
Akshay Pokalwar <akshaypklwr@gmail.com>
2018-10-02 13:41:51 +0530

    Formatted code

--- app/src/androidTest/java/deva/com/homeboy/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/deva/com/homeboy/ExampleInstrumentedTest.java
@@ -20,7 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("deva.com.homeboy", appContext.getPackageName());
     }
 }
